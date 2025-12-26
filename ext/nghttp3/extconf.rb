3378e63bf665c57fa0ffaf156498e8753da7f62f
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-12-26 16:28:35 +0900

    Check nghttp3 lib existence in extconf.rb

--- ext/nghttp3/extconf.rb
+++ ext/nghttp3/extconf.rb
@@ -7,4 +7,12 @@ require "mkmf"
 # selectively, or entirely remove this flag.
 append_cflags("-fvisibility=hidden")
 
+unless have_library("nghttp3", "nghttp3_version")
+  abort "nghttp3 library not found"
+end
+
+unless have_header("nghttp3/nghttp3.h")
+  abort "nghttp3/nghttp3.h not found"
+end
+
 create_makefile("nghttp3/nghttp3")
